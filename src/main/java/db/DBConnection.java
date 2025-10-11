86ecd536b28bcfefcbf31f5aded9b183083e778a
TrungNguyen <trunguyen.104@gmail.com>
2025-10-11 23:27:40 +0700

    add ViewProfileAuthorByReader.html

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -5,11 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         try {
             String url = "jdbc:sqlserver://localhost:1433;databaseName=JoyLeeBook;encrypt=true;trustServerCertificate=true;";
             String username = "sa";
-            String password = "04072005Toan";
+            String password = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
