001359af50c3488a134a20f7e468ba433e9c77e4
Toan <tktoan10a1@gmail.com>
2025-10-13 17:17:51 +0700

    Clean code, remove warning, unified code format

--- src/main/java/controller/generalController/LibraryServlet.java
+++ src/main/java/controller/generalController/LibraryServlet.java
@@ -10,50 +10,34 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Series;
 import services.series.SeriesServices;
+import utils.ValidationInput;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/library")
 public class LibraryServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String userIdParam = request.getParameter("userId");
-        String userIdParam = "1";
-        int userId = 0;
-        if (userIdParam != null && !userIdParam.isEmpty()) {
-            try {
-                userId = Integer.parseInt(userIdParam);
-            } catch (NumberFormatException e) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid userId");
-                return;
-            }
-        } else {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing userId");
-            return;
-        }
+        String userIdParam = request.getParameter("userId");
+
+        int userId = ValidationInput.isPositiveInteger(userIdParam) ? Integer.parseInt(userIdParam) : 1;
         try {
             Connection conection = DBConnection.getConnection();
             SeriesDAO  seriesDAO = new SeriesDAO(conection);
             ChapterDAO chapterDAO = new ChapterDAO(conection);
-            List<SeriesInfoDTO> savedSeries = new ArrayList<>();
-            SeriesServices seriesServices = new SeriesServices(conection);
-            for (Series series : seriesDAO.getSeriesByUserId(userId)) {
-                savedSeries.add(seriesServices.buildSeriesInfoDTO(series));
-            }
-            List<ChapterItemDTO> historyChapters = chapterDAO.getReadingHistoryChapters(userId, 0, Integer.MAX_VALUE, null);
 
+            SeriesServices seriesServices = new SeriesServices(conection);
+            List<SeriesInfoDTO> savedSeries = seriesServices.buildSeriesInfoDTOList(seriesDAO.getSeriesByUserId(userId));
+
+            List<ChapterItemDTO> historyChapters = chapterDAO.getReadingHistoryChapters(userId, 0, Integer.MAX_VALUE, null);
 
             request.setAttribute("savedSeries", savedSeries);
             request.setAttribute("historyChapters", historyChapters);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
         request.getRequestDispatcher("/WEB-INF/views/general/Library.jsp").forward(request, response);
--- src/main/java/controller/seriesController/SeriesDetailServlet.java
+++ src/main/java/controller/seriesController/SeriesDetailServlet.java
@@ -9,15 +9,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Chapter;
+import services.chapter.ChapterServices;
 import services.series.SeriesServices;
+import utils.ValidationInput;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/series-detail")
@@ -27,62 +25,28 @@ public class SeriesDetailServlet extends HttpServlet {
         String seriesIdParam = request.getParameter("seriesId");
 
 
-        int seriesId = 0;
-        if (seriesIdParam != null && !seriesIdParam.isEmpty()) {
-            try {
-                seriesId = Integer.parseInt(seriesIdParam);
-            } catch (NumberFormatException e) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid seriesId");
-                return;
-            }
-        } else {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing seriesId");
-            return;
-        }
+        int seriesId = ValidationInput.isPositiveInteger(seriesIdParam) ? Integer.parseInt(seriesIdParam) : 0;
 
         try {
             Connection connection = DBConnection.getConnection();
-            LikesDAO likesDAO = new LikesDAO(connection);
             SeriesDAO seriesDAO = new SeriesDAO(connection);
             ChapterDAO chapterDAO = new ChapterDAO(connection);
+
             SeriesServices seriesServices = new SeriesServices(connection);
             SeriesInfoDTO seriesInfoDTO = seriesServices.buildSeriesInfoDTO(seriesDAO.findById(seriesId));
-            List<ChapterInfoDTO> chapterInfoDTOList = new ArrayList<>();
-            for (Chapter chapter : chapterDAO.findChapterBySeriesId(seriesInfoDTO.getSeriesId())) {
-                ChapterInfoDTO chapterInfoDTO = new ChapterInfoDTO();
-                chapterInfoDTO.setChapterId(chapter.getChapterId());
-                chapterInfoDTO.setTitle(chapter.getTitle());
-                chapterInfoDTO.setChapterNumber(chapter.getChapterNumber());
-                chapterInfoDTO.setUpdatedAt(calculateTimeAgo(chapter.getUpdatedAt()));
-                chapterInfoDTO.setTotalLikes(likesDAO.countByChapter(chapter.getChapterId()));
-                chapterInfoDTOList.add(chapterInfoDTO);
-            }
+
+            ChapterServices chapterServices = new ChapterServices();
+            List<ChapterInfoDTO> chapterInfoDTOList = chapterServices.buildChapterInfoDTOList(chapterDAO.findChapterBySeriesId(seriesInfoDTO.getSeriesId()), connection);
+
             request.setAttribute("seriesInfoDTO", seriesInfoDTO);
             request.setAttribute("chapterInfoDTOList", chapterInfoDTOList);
-
-            request.setAttribute("pageTitle", "Series Detail");
-//            request.setAttribute("contentPage", "/WEB-INF/views/series/SeriesDetail.jsp");
-            request.setAttribute("contentPage", "/testing.jsp");
-            request.getRequestDispatcher("/WEB-INF/views/components/_layoutUser.jsp").forward(request, response);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
+            request.getRequestDispatcher("/WEB-INF/views/series/SeriesDetail.jsp").forward(request, response);
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
 
     }
 
 
-    private String calculateTimeAgo(LocalDateTime updatedAt) {
-        Duration duration = Duration.between(updatedAt, LocalDateTime.now());
-        if (duration.toDays() > 0) {
-            return duration.toDays() + " days ago";
-        } else if (duration.toHours() > 0) {
-            return duration.toHours() + " hours ago";
-        } else if (duration.toMinutes() > 0) {
-            return duration.toMinutes() + " minutes ago";
-        } else {
-            return "just now";
-        }
-    }
+
 }
