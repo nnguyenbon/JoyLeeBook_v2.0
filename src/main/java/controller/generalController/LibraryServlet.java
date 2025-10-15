346419c81c63eaad60eef780e5e84d27b04e4699
Toan <tktoan10a1@gmail.com>
2025-10-15 08:40:54 +0700

    Update way to code based on services class

--- src/main/java/controller/generalController/LibraryServlet.java
+++ src/main/java/controller/generalController/LibraryServlet.java
@@ -1,45 +1,32 @@
 package controller.generalController;
 
-import dao.ChapterDAO;
-import dao.SeriesDAO;
 import db.DBConnection;
-import dto.chapter.ChapterItemDTO;
-import dto.series.SeriesInfoDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import services.chapter.ChapterServices;
 import services.series.SeriesServices;
 import utils.ValidationInput;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 
 @WebServlet("/library")
 public class LibraryServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String userIdParam = request.getParameter("userId");
-
-        int userId = ValidationInput.isPositiveInteger(userIdParam) ? Integer.parseInt(userIdParam) : 1;
+        int userId = ValidationInput.isPositiveInteger(request.getParameter("userId")) ? Integer.parseInt(request.getParameter("userId")) : 1;
         try {
-            Connection conection = DBConnection.getConnection();
-            SeriesDAO  seriesDAO = new SeriesDAO(conection);
-            ChapterDAO chapterDAO = new ChapterDAO(conection);
+            ChapterServices chapterServices = new ChapterServices(DBConnection.getConnection());
+            SeriesServices seriesServices = new SeriesServices(DBConnection.getConnection());
 
-            SeriesServices seriesServices = new SeriesServices(conection);
-            List<SeriesInfoDTO> savedSeries = seriesServices.buildSeriesInfoDTOList(seriesDAO.getSeriesByUserId(userId));
-
-            List<ChapterItemDTO> historyChapters = chapterDAO.getReadingHistoryChapters(userId, 0, Integer.MAX_VALUE, null);
-
-            request.setAttribute("savedSeries", savedSeries);
-            request.setAttribute("historyChapters", historyChapters);
+            request.setAttribute("savedSeries", seriesServices.savedSeriesFromUser(userId));
+            request.setAttribute("historyChapters", chapterServices.historyChaptersFromUser(userId, 0, Integer.MAX_VALUE, null));
+            request.getRequestDispatcher("/WEB-INF/views/general/Library.jsp").forward(request, response);
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-        request.getRequestDispatcher("/WEB-INF/views/general/Library.jsp").forward(request, response);
     }
 }
