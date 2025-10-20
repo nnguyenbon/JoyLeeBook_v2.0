fc3459881f5c05597e501ea2702e9fd00d858537
Toan <tktoan10a1@gmail.com>
2025-10-20 10:54:05 +0700

    Update tree struture

--- src/main/java/controller/reportController/ViewReportDetailServlet.java
+++ src/main/java/controller/reportController/ViewReportDetailServlet.java
@@ -6,26 +6,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import services.report.ReportServices;
+import utils.ValidationInput;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet("/report")
-public class ViewReportServlet extends HttpServlet {
+@WebServlet("/report-detail")
+public class ViewReportDetailServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String type = request.getParameter("type") == null ? "" : request.getParameter("type");
+        String type = request.getParameter("type");
+        int type_id = ValidationInput.isPositiveInteger(request.getParameter("type_id")) ?  Integer.parseInt(request.getParameter("type_id")) : 1;
         try {
             ReportServices reportServices = new ReportServices();
-            if (reportServices.handleRedirect(type, request, response)) {
-                return;
-            }
-
-            request.setAttribute("type", type != null ? type : "");
-            request.getRequestDispatcher("/WEB-INF/views/general/ReportList.jsp").forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/views/report/ChapterReportDetail.jsp").forward(request, response);
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
