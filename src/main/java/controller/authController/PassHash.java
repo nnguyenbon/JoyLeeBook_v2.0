59fefc5dac4e893392d6deac0378c7948b7089cc
haido07 <haido504001@gmail.com>
2025-11-05 07:19:09 +0700

    update and fix structure

--- src/main/java/controller/authController/PassHash.java
+++ src/main/java/controller/authController/PassHash.java
@@ -6,7 +6,7 @@ import org.mindrot.jbcrypt.BCrypt;
  * Example class to demonstrate password hashing using BCrypt.
  * This class hashes a sample password and prints the hashed value.
  */
-public class PasswordHashExample {
+public class PassHash {
     public static void main(String[] args) {
         String password = "123"; // mật khẩu gốc
         String hashed = BCrypt.hashpw(password, BCrypt.gensalt(10));
